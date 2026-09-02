d937d6d1a68b63a1788d6d8295f84540273c627a
Vaibhav Sharad Mali <malivaibhav436@gmail.com>
2026-09-02 22:49:32 +0530

    Time: 7 ms (87.02%), Space: 20.4 MB (79.19%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,17 +1,19 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        hashmap={}
-        dupl=-1
-        missing=-1
-        for i in nums:
-            hashmap[i]=hashmap.get(i,0)+1
-            if hashmap[i]==2:
-                dupl=i
-        for i in range(1, len(nums) + 1):
-            if i not in hashmap:
-                missing=i
-                break
-        return [dupl, missing]
+        n = len(nums)
+        counts = [0] * (n + 1)
+        for num in nums:
+            counts[num] += 1
+
+        missing = -1
+        duple = -1
+        for i in range(1, n + 1):
+            if counts[i] > 1:
+                duplicate = i
+            elif counts[i] < 1:
+                missing = i
+                
+        return duplicate, missing
 
 
         
